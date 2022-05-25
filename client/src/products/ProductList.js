fe3dd78faa8276605fbf880e2c47f55e22eaaa9e
AMITKESARI2000 <kesari.amit33@gmail.com>
2022-05-25 21:46:33 +0530

    item browse and filters

--- client/src/products/ProductList.js
+++ client/src/products/ProductList.js
@@ -6,109 +6,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ScrollToTopOnMount from '../template/ScrollToTopOnMount';
 import axios from 'axios';
 
-let categories = [];
-let brands = [];
-let manufacturers = [];
-
-function FilterMenuLeft() {
-    return (
-        <ul className="list-group list-group-flush rounded">
-            <li className="list-group-item d-none d-lg-block">
-                <h5 className="mt-1 mb-2">Browse</h5>
-                <div className="d-flex flex-wrap my-2">
-                    {categories.map((v, i) => {
-                        return (
-                            <Link
-                                key={i}
-                                to="/products"
-                                className="btn btn-sm btn-outline-dark rounded-pill me-2 mb-2"
-                                replace
-                            >
-                                {v}
-                            </Link>
-                        );
-                    })}
-                </div>
-            </li>
-            <li className="list-group-item">
-                <h5 className="mt-1 mb-1">Brands</h5>
-                <div className="d-flex flex-column">
-                    {brands.map((v, i) => {
-                        return (
-                            <div key={i} className="form-check">
-                                <input
-                                    className="form-check-input"
-                                    type="checkbox"
-                                />
-                                <label
-                                    className="form-check-label"
-                                    htmlFor="flexCheckDefault"
-                                >
-                                    {v}
-                                </label>
-                            </div>
-                        );
-                    })}
-                </div>
-            </li>
-            <li className="list-group-item">
-                <h5 className="mt-1 mb-1">Manufacturers</h5>
-                <div className="d-flex flex-column">
-                    {manufacturers.map((v, i) => {
-                        return (
-                            <div key={i} className="form-check">
-                                <input
-                                    className="form-check-input"
-                                    type="checkbox"
-                                />
-                                <label
-                                    className="form-check-label"
-                                    htmlFor="flexCheckDefault"
-                                >
-                                    {v}
-                                </label>
-                            </div>
-                        );
-                    })}
-                </div>
-            </li>
-            <li className="list-group-item">
-                <h5 className="mt-1 mb-2">Price Range</h5>
-                <div className="d-grid d-block mb-3">
-                    <div className="form-floating mb-2">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Min"
-                            defaultValue="100000"
-                        />
-                        <label htmlFor="floatingInput">Min Price</label>
-                    </div>
-                    <div className="form-floating mb-2">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Max"
-                            defaultValue="500000"
-                        />
-                        <label htmlFor="floatingInput">Max Price</label>
-                    </div>
-                    <button className="btn btn-dark">Apply</button>
-                </div>
-            </li>
-        </ul>
-    );
-}
+let categories = [
+    "Men's Wear",
+    " Women's Wear",
+    'Electronics',
+    'Daily Items',
+    'Food',
+    'Accessories',
+];
+let brands = ['Levi', 'Apple', 'Adidas', 'Nike', 'HP'];
+let manufacturers = ['Apple', 'Samsung', 'Sonata', 'Tata', 'Bajaj'];
+let globalMinPrice = 100;
+let globalMaxPrice = 100000;
 
 const ProductList = () => {
+    const [minPrice, setMinPrice] = useState(globalMinPrice);
+    const [maxPrice, setMaxPrice] = useState(globalMaxPrice);
+
     const [viewType, setViewType] = useState({ grid: true });
     const [productsList, setProductsList] = useState([]);
+    const [manufacturersList, setManufacturersList] = useState(manufacturers);
 
-    const getItems = (_) => {
-        axios.get(`${process.env.REACT_APP_BACKEND_URL}`).then((res) => {
-            setProductsList(res.data);
-            productsList.forEach(
-                function (item, index) {
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_BACKEND_URL}`)
+            .then((res) => {
+                setProductsList(res.data);
+                productsList.forEach(function (item, index) {
                     categories.push(item.category);
                     brands.push(item.brand);
                     manufacturers.push(item.manufacturer);
@@ -116,11 +40,8 @@ const ProductList = () => {
                     brands = [...new Set(brands)];
                     manufacturers = [...new Set(manufacturers)];
                 });
-        });
-    };
-
-    useEffect(() => {
-        getItems();
+            })
+            .catch((err) => console.log(err));
     }, []);
 
     const changeViewType = () => {
@@ -144,7 +65,7 @@ const ProductList = () => {
                         </Link>
                     </li>
                     <li className="breadcrumb-item active" aria-current="page">
-                        Cases &amp; Covers
+                        Carousel
                     </li>
                 </ol>
             </nav>
@@ -190,7 +111,8 @@ const ProductList = () => {
                             data-bs-parent="#accordionFilter"
                         >
                             <div className="accordion-body p-0">
-                                <FilterMenuLeft />
+                                {/* TODO: add heree */}
+                                {/* <FilterMenuLeft /> */}
                             </div>
                         </div>
                     </div>
@@ -200,7 +122,110 @@ const ProductList = () => {
             <div className="row mb-4 mt-lg-3">
                 <div className="d-none d-lg-block col-lg-3">
                     <div className="border rounded shadow-sm">
-                        <FilterMenuLeft />
+                        {/* ===FILTER MENU=== */}
+                        <ul className="list-group list-group-flush rounded">
+                            <li className="list-group-item d-none d-lg-block">
+                                <h5 className="mt-1 mb-2">Browse</h5>
+                                <div className="d-flex flex-wrap my-2">
+                                    {categories.map((v, i) => {
+                                        return (
+                                            <Link
+                                                key={i}
+                                                to="/products"
+                                                className="btn btn-sm btn-outline-dark rounded-pill me-2 mb-2"
+                                                replace
+                                            >
+                                                {v}
+                                            </Link>
+                                        );
+                                    })}
+                                </div>
+                            </li>
+                            <li className="list-group-item">
+                                <h5 className="mt-1 mb-1">Brands</h5>
+                                <div className="d-flex flex-column">
+                                    {brands.map((v, i) => {
+                                        return (
+                                            <div key={i} className="form-check">
+                                                <input
+                                                    className="form-check-input"
+                                                    type="checkbox"
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="flexCheckDefault"
+                                                >
+                                                    {v}
+                                                </label>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </li>
+                            <li className="list-group-item">
+                                <h5 className="mt-1 mb-1">Manufacturers</h5>
+                                <div className="d-flex flex-column">
+                                    {manufacturers.map((v, i) => {
+                                        return (
+                                            <div key={i} className="form-check">
+                                                <input
+                                                    className="form-check-input"
+                                                    type="checkbox"
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="flexCheckDefault"
+                                                >
+                                                    {v}
+                                                </label>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </li>
+                            <li className="list-group-item">
+                                <h5 className="mt-1 mb-2">Price Range</h5>
+                                <div className="d-grid d-block mb-3">
+                                    <div className="form-floating mb-2">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Min"
+                                            defaultValue={minPrice}
+                                            value={minPrice}
+                                            onChange={(e) => {
+                                                setMinPrice(e.target.value);
+                                            }}
+                                        />
+                                        <label htmlFor="floatingInput">
+                                            Min Price
+                                        </label>
+                                    </div>
+                                    <div className="form-floating mb-2">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Max"
+                                            defaultValue={maxPrice}
+                                            value={maxPrice}
+                                            onChange={(e) => {
+                                                setMaxPrice(e.target.value);
+                                            }}
+                                        />
+                                        <label htmlFor="floatingInput">
+                                            Max Price
+                                        </label>
+                                    </div>
+                                    <button
+                                        className="btn btn-dark"
+                                        // onClick={handlePriceRange}
+                                    >
+                                        Apply
+                                    </button>
+                                </div>
+                            </li>
+                        </ul>
+                        {/* ===FILTER MENU=== */}
                     </div>
                 </div>
                 <div className="col-lg-9">
@@ -255,10 +280,20 @@ const ProductList = () => {
                                     : 'row-cols-xl-2')
                             }
                         >
-                            {productsList.map((product, index) => {
-                                return <Product key={index} product={product} />;
-                            })}
-                            
+                            {productsList
+                                .filter(
+                                    (product, index) =>
+                                        product.price <= maxPrice &&
+                                        product.price >= minPrice
+                                )
+                                .map((product, index) => {
+                                    return (
+                                        <Product
+                                            key={index}
+                                            product={product}
+                                        />
+                                    );
+                                })}
                         </div>
                         <div className="d-flex align-items-center mt-auto">
                             <span className="text-muted small d-none d-md-inline">
